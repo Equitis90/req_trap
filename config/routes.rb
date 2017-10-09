b6338f90a5db6267b7f96c608f74a12c0e90c56f
Vladyslav Sydorenko <svuitis@gmail.com>
2017-10-09 20:58:30 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   root to: 'trap#index'
 
   match '/:trap_id', to: 'trap#create', via: :all
-  match '/:trap_id/requests', to: 'trap#requests', via: :all
+  match '/:trap_id/requests', to: 'trap#requests', via: :get
   get 'trap/render_request'
 end
